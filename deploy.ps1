7a226153b9acf54d466103b09e2bfe7e9d9a61dd
Vikas Tiwari <vikastiwari1045@gmail.com>
2026-05-04 08:23:32 +0530

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,9 +0,0 @@
-gcloud run deploy civic-clarity `
-  --source . `
-  --region us-central1 `
-  --platform managed `
-  --allow-unauthenticated `
-  --port 8080 `
-  --memory 512Mi `
-  --set-env-vars "NODE_ENV=production,FIREBASE_PROJECT_ID=virtualpromptwars-eb2fd,VERTEX_AI_PROJECT=virtualpromptwars-eb2fd,VERTEX_AI_LOCATION=us-central1,EMAIL_USER=vikastiwari1045@gmail.com,EMAIL_APP_PASSWORD=bcgo rpvb nzac auaw,EMAIL_FROM_NAME=ElectionBond,ALLOWED_ORIGINS=https://civic-clarity-uc.a.run.app" `
-  --project virtualpromptwars-eb2fd
